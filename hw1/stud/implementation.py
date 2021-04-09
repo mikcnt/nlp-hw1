5a83d4c601504dbe028c35fbfe7f735a98f48b63
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2021-04-09 17:40:42 +0200

    Add checkpoint class in main; add first template to test model in hw1

--- hw1/stud/implementation.py
+++ hw1/stud/implementation.py
@@ -3,11 +3,13 @@ from typing import List, Tuple, Dict
 
 from model import Model
 
+import pickle
+import torch
+from torch import nn
+import os
 
 def build_model(device: str) -> Model:
-    # STUDENT: return StudentModel()
-    # STUDENT: your model MUST be loaded on the device "device" indicates
-    return RandomBaseline()
+    return StudentModel().to(device)
 
 
 class RandomBaseline(Model):
@@ -28,11 +30,33 @@ class RandomBaseline(Model):
 
 
 class StudentModel(Model):
-    
-    # STUDENT: construct here your model
-    # this class should be loading your weights and vocabulary
+    def __init__(self):
+        super().__init__()
+        self.model = Model()
+        weights = torch.load(
+            "model/022.pth", map_location=lambda storage, loc: storage
+        )
+        self.model.load_state_dict(weights["model_state_dict"])
+        with open('model/vocabulary.pkl', 'rb') as f:
+            self.vocabulary = pickle.load(f)
 
     def predict(self, sentence_pairs: List[Dict]) -> List[Dict]:
-        # STUDENT: implement here your predict function
-        # remember to respect the same order of sentences!
-        pass
+        sentences = []
+        for sentence in sentence_pairs:
+            s1 = self.sentence2vector(sentence['sentence1'])
+            s2 = self.sentence2vector(sentence['sentence2'])
+            sentence_vector = torch.cat((s1, s2)).unsqueeze(0)
+            sentences.append(sentence_vector)
+        sentences = torch.stack(sentences)
+        self.model.eval()
+        predicted = self.model(sentences).round().int()
+        return [str(bool(x.item())) for x in predicted]
+    
+    def sentence2vector(self, sentence: str):
+        sentences_word_vector = [self.vocabulary[w] for w in sentence.split(' ') if w in self.vocabulary]
+        
+        if len(sentences_word_vector) == 0:
+            return None
+
+        sentences_word_vector = torch.stack(sentences_word_vector)
+        return torch.mean(sentences_word_vector, dim=0)
